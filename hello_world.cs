d489efe8d84a81ce316ffc4a02bd861e87ddf2e0
Tasha Pais <32210691+tashapais@users.noreply.github.com>
2024-02-11 11:22:29 -0500

    use both code snippets

--- hello_world.cs
+++ hello_world.cs
@@ -1,12 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-public class ChatGPTAPI : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     private readonly string apiKey = "YOUR_API_KEY_HERE";
     private readonly string apiURL = "https://api.openai.com/v1/chat/completions";
 
+    // Start is called before the first frame update
     async void Start()
     {
         string prompt = "Hello, world!";
@@ -14,12 +17,18 @@ public class ChatGPTAPI : MonoBehaviour
         Debug.Log(response);
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     public async Task<string> GetChatGPTResponse(string prompt)
     {
         using (HttpClient client = new HttpClient())
         {
             client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
-            
+
             var requestData = new
             {
                 model = "text-davinci-003",
@@ -31,7 +40,7 @@ public class ChatGPTAPI : MonoBehaviour
 
             HttpResponseMessage response = await client.PostAsync(apiURL,
                 new StringContent(json, System.Text.Encoding.UTF8, "application/json"));
-            
+
             string result = await response.Content.ReadAsStringAsync();
             return result;
         }
